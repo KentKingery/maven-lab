40c4fe142eb846d051ae9126ead80ebdccb8b1da
Kent Kingery <kent.kingery@vertexinc.com>
2023-12-13 10:54:32 -0600

    Safety commit

---
+++ demo-app/src/main/java/io/acmecode/demo/App.java
@@ -0,0 +1,14 @@
+package io.acmecode.demo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "AcmeCode Demo App 1.0" );
+        System.out.println( "======================================================================");
+    }
+}
